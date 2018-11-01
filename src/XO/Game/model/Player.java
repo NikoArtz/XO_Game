23962ca86d3f98b0f264f1880d0bdf028eb4d22b
Mikita Artsiomchyk <nikitaartsiomchyk@gmail.com>
2018-11-01 20:24:19 +0200

    Figure.class and Player has been added

---
+++ src/XO/Game/model/Player.java
@@ -0,0 +1,19 @@
+package XO.Game.model;
+
+public class Player {
+    private final String name;
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
