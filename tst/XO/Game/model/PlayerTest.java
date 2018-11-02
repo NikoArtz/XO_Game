1499507dae78b1712d1bd7a37874bc5bed319841
Mikita Artsiomchyk <nikitaartsiomchyk@gmail.com>
2018-11-02 12:36:48 +0200

    Tests  has been added

--- tst/XO/Game/model/PlayerTest.java
+++ tst/XO/Game/model/PlayerTest.java
@@ -2,15 +2,27 @@ package XO.Game.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class PlayerTest {
 
     @Test
     public void getName() {
+        final String inputValue = "Nikita";
+        final String expectedValue = inputValue;
+        final Player player = new Player(inputValue, null);
+        final String actualValue = player.getName();
+        assertEquals(expectedValue,actualValue);
     }
+
+
 
     @Test
     public void getFigure() {
+        final Figure inputValue1 = Figure.X;
+        final Figure expectedValue1 = inputValue1;
+        final Player player = new Player(null, expectedValue1);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue1,actualValue);
     }
 }
